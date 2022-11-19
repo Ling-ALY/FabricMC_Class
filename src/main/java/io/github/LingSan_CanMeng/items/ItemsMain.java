fd0834ed75ccbbfc0d49dbdbd74c342eb8b59960
LingSan_CanMeng <yxp20050828@163.com>
2022-11-19 20:15:18 +0800

    第二课-添加基础物品

---
+++ src/main/java/io/github/LingSan_CanMeng/items/ItemsMain.java
@@ -0,0 +1,22 @@
+package io.github.LingSan_CanMeng.items;
+
+import io.github.LingSan_CanMeng.Main;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ItemsMain {
+
+    public static final Item Amber = Registry.register(Registry.ITEM,
+            new Identifier(Main.MOD_ID,
+                    "amber"),
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16)
+            ));
+
+    public static void registerItems(){
+        Main.LOGGER.debug(Main.MOD_ID + "正在注册物品");
+    }
+
+}
