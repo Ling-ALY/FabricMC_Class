b1ba65dbd089d11b287617518213786fb0230ff8
LING-ALY <yxp20050828@163.com>
2022-11-19 12:26:24 +0800

    第一课-准备工作

---
+++ src/main/java/io/github/LingSan_CanMeng/Main.java
@@ -0,0 +1,16 @@
+package io.github.LingSan_CanMeng;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+
+	public static final String MOD_ID = "lscm";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
