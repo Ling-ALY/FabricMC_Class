1163e9b3724087f0bd64e75d2fc805389681d354
LingSan_CanMeng <yxp20050828@163.com>
2022-11-21 00:26:21 +0800

    第二点五课-基础物品通用公式

--- src/main/java/io/github/LingSan_CanMeng/items/ItemsMain.java
+++ src/main/java/io/github/LingSan_CanMeng/items/ItemsMain.java
@@ -9,10 +9,20 @@ import net.minecraft.util.registry.Registry;
 
 public class ItemsMain {
 
-    public static final Item Amber = Registry.register(Registry.ITEM,
+    /**
+    public static final Item amber = Registry.register(Registry.ITEM,
             new Identifier(Main.MOD_ID, "amber"),
             new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16)
             ));
+     **/
+
+    public static final Item amber = registerItem("amber",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16)));
+
+    private static Item registerItem(String itemName, Item item){
+        return Registry.register(Registry.ITEM,
+                new Identifier(Main.MOD_ID, itemName), item);
+    }
 
     public static void registerItems(){
         Main.LOGGER.debug(Main.MOD_ID + "正在注册物品");
