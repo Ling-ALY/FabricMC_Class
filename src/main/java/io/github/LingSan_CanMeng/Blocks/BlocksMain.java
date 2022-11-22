acf41ae15ce975a1e47493807077f82130db2782
LingSan_CanMeng <yxp20050828@163.com>
2022-11-23 01:00:34 +0800

    第六课-设置方块需要工具以及方块的掉落物

--- src/main/java/io/github/LingSan_CanMeng/Blocks/BlocksMain.java
+++ src/main/java/io/github/LingSan_CanMeng/Blocks/BlocksMain.java
@@ -6,21 +6,26 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 public class BlocksMain {
 
     public static final Block zincBlock = registerBlock("zinc_block",
-            new Block(FabricBlockSettings.of(Material.METAL)),
+            new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(6f)),
             ItemGroupsMain.lscm);
     public static final Block zincOre = registerBlock("zinc_ore",
-            new Block(FabricBlockSettings.of(Material.STONE)),
+            new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3f),
+                    UniformIntProvider.create(1,2)),
             ItemGroupsMain.lscm);
     public static final Block deepslateZincOre = registerBlock("deepslate_zinc_ore",
-            new Block(FabricBlockSettings.of(Material.STONE)),
+            new OreBlock(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.DEEPSLATE).requiresTool().strength(4.5f),
+                    UniformIntProvider.create(2,3)),
             ItemGroupsMain.lscm);
 
     public static Block registerBlock(String blockName,Block block, ItemGroup itemGroup){
